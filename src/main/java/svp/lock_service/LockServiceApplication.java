da11d2281c4e4474e0fa42958a367245e510af61
Dmitrii Maksimov <maksimow.dmitrij@yandex.ru>
2019-04-25 10:25:46 +0300

    Initialize spring project

---
+++ src/main/java/svp/lock_service/LockServiceApplication.java
@@ -0,0 +1,13 @@
+package svp.lock_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/svp/lock_service/LockServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package svp.lock_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LockServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
