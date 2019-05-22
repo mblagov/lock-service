24562d30ad319cd197ba658ab2f38f2f80b7e56a
Андрей Шубин <shubandrey@gmail.com>
2019-05-22 17:05:01 +0300

    fix required table name to schema.tableName

--- src/main/java/svp/lock_service/common/HiveHelper.java
+++ src/main/java/svp/lock_service/common/HiveHelper.java
@@ -22,10 +22,18 @@ public class HiveHelper {
         return connection.getMetaData().getTables(null, null, null, null).getString(1);
     }
 
-    public boolean isTableExists(String tableName) throws SQLException {
+    public boolean isTableExists(String fullTableName) throws SQLException {
+        String[] tableNameParts = fullTableName.split(".");
+        String database = tableNameParts[0];
+        String tableName = tableNameParts[1];
+        getConnection(database);
         DatabaseMetaData metadata = connection.getMetaData();
         ResultSet res = metadata.getTables(null, null, tableName, null);
         return res.next();
+    }
+
+    private void getConnection(String database) throws SQLException{
+        connection = DriverManager.getConnection("jdbc:hive2://localhost:10000/" + database, "students", "students");
     }
 
 }
